94da37be0ea3fe8197485d4277471f718f742cad
Sujal Memane <sujal.memane23@gmail.com>
2025-09-21 01:10:24 +0530

    Delete app/src/main/java/com/coursecampus/athlete_connect directory
    remove old packages

--- app/src/main/java/com/coursecampus/athlete_connect/FitnessLabApplication.kt
+++
@@ -1,8 +0,0 @@
-package com.coursecampus.athleteconnect
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class FitnessLabApplication : Application()
-
